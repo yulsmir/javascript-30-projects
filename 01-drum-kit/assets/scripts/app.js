145d64a27f21452443dc04c35130e9a536b18a28
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-03-30 00:06:09 +0200

    Add playSound, removeTrunsition functions

--- 01-drum-kit/assets/scripts/app.js
+++ 01-drum-kit/assets/scripts/app.js
@@ -1,11 +1,21 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
-  window.addEventListener('keydown', function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
 
-    if (!audio) return;
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  console.log(e.propertyName);
+  this.classList.remove('playing');
+}
 
-    audio.play();
-  });
-});
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
