0e956455e7054778bcf04a941966bdcc38340f6d
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-04 20:36:03 +0200

    Add digital clock to css-js-task

--- 02-css-and-js-clock/app.js
+++ 02-css-and-js-clock/app.js
@@ -2,11 +2,19 @@ const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+const secondsDigital = document.querySelector('.seconds');
+const minutesDigital = document.querySelector('.minutes');
+const hoursDigital = document.querySelector('.hours');
+
+const currentDate = document.querySelector('.current-date');
+
 const setDate = () => {
   const now = new Date();
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
+
+  const date = now.getDate();
 
   const secondsDegrees = (seconds / 60) * 360 + 90;
   const minsDegrees = (minutes / 60) * 360 + 90;
@@ -15,6 +23,10 @@ const setDate = () => {
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  hoursDigital.textContent = `${hours}:`;
+  minutesDigital.textContent = `${minutes}:`;
+  secondsDigital.textContent = seconds;
 
   console.log(seconds);
 };
