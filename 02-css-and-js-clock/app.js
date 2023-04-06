a02007d5b6092f40310f1c72e50c6fad5f6fe035
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-06 02:51:52 +0200

    Fix style of clock elements

--- 02-css-and-js-clock/app.js
+++ 02-css-and-js-clock/app.js
@@ -2,6 +2,10 @@ const secondsDigital = document.querySelector('.seconds');
 const minutesDigital = document.querySelector('.minutes');
 const hoursDigital = document.querySelector('.hours');
 
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 const updateTime = () => {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -11,6 +15,14 @@ const updateTime = () => {
   hoursDigital.textContent = `${addZeroBeforeNumber(hours)}:`;
   minutesDigital.textContent = `${addZeroBeforeNumber(minutes)}:`;
   secondsDigital.textContent = `${addZeroBeforeNumber(seconds)}`;
+
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minsDegrees = (minutes / 60) * 360 + 90;
+  const hoursDegrees = (hours / 12) * 360 + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 };
 
 const addZeroBeforeNumber = (number) => {
